c5073c17c55524ae54e22a2067e9e06844800693
tomer0702 <73163395+tomer0702@users.noreply.github.com>
2020-10-23 13:28:13 +0530

    Update permision.js

--- extra/utils/permision.js
+++ extra/utils/permision.js
@@ -1,10 +1,11 @@
 
 let permissions= {
     'getUsers': {
-        'all': ['head-trainer'],
-        'read': ['trainee', 'trainer'],
-        'write': ['trainer'],
-        'delete': [],
+
+        'all' : ['head-trainer'],
+        'read': ['trainee', 'trainer','head-trainer'],
+        'write': ['trainer','head-trainer'],
+        'delete': ['head-trainer'],
     }
 }
 
@@ -13,48 +14,48 @@ function hasPermission(moduleName,role,permissionType)
    
     if(permissions[moduleName]!== undefined)
     {
-        console.log("Module Name found")
+        console.log("Module Name ="+moduleName)
         if(permissions[moduleName][permissionType]!==undefined)
         {
-            console.log("Permission Type found")
+            console.log("Permission Type ="+permissionType)
             if(permissions[moduleName][permissionType].length>0)
             {
                 if(permissions[moduleName][permissionType].indexOf(role)!==-1)
                 {
-                    console.log("Role found")
+                    console.log("Role ="+role)
                     return true;
                 }
                 else
                 {
-                    console.log("Role not found")
+                    console.log("Role not matched="+role)
                     return false;
                 }
             }
             else
             {
                 console.log("No role available for delete")
-                if(role==='')
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+               
             }
         }
         else
         {
-            console.log("Permission Type not found")
+            console.log("Permission Type not matched="+permissionType)
             return false;
         }    
     }
     else
     {
-        console.log("Module Name is not found")
+        console.log("Module Name is not found="+moduleName)
         return false;
     }
 }
 
+console.log(hasPermission(' gt','head-trainer','write'));
+
+console.log(hasPermission('getUsers','trainer','read'));
+
+console.log(hasPermission('getUsers','trainer','delete'));
+
+console.log(hasPermission('getUsers','tr','delete'));
+
 console.log(hasPermission('getUsers','head-trainer','all'));
-console.log(hasPermission('getUsers','trainer','write'));
