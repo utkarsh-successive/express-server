88826a97908f8d158bbcadac969fc01c9792be51
tomer0702 <73163395+tomer0702@users.noreply.github.com>
2020-10-30 15:39:12 +0530

    Update errorHandler.ts

--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { IError } from './interface';
 export default(err: IError, req: Request, res: Response, next: NextFunction) => {
-    res.json(
+      if (res.headersSent) {
+        return next(err);
+    }
+    next(),
+    res.status (err.code).json(
         {
             error: err.error,
-            message: err.message || 'error',
+            message: err.message || 'file not found',
             status: err.code,
             timestamp: new Date()
         }
