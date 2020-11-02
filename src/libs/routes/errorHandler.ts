ed154b8b90f506c1b9bdf68d0af3f300a6b770e0
tomer0702 <73163395+tomer0702@users.noreply.github.com>
2020-11-02 11:51:52 +0530

    Update errorHandler.ts

--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -4,7 +4,6 @@ export default(err: IError, req: Request, res: Response, next: NextFunction) => 
       if (res.headersSent) {
         return next(err);
     }
-    next(),
     res.status (err.code).json(
         {
             error: err.error,
