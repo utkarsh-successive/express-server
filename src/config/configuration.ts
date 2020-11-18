a106e79cc7d3bc8dc5c18196580610364f85a6dc
tomer0702 <utkarsh.tomer@successive.tech>
2020-11-18 11:24:37 +0530

    modified lint feature/40012

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,16 +1,10 @@
-
 import * as dotenv from 'dotenv';
-//console.log(dotenv);
 import { Iconfig } from './IConfig';
-// tslint:disable-next-line:no-var-requires
-const envVars = require('dotenv').config();
-//console.log('Inside config', envVars);
-const config: Iconfig = envVars.parsed;
-Object.freeze( config );
+const enVars = dotenv.config().parsed;
+const config = enVars;
+Object.freeze(config);
+
 export default config;
-
-
-
 
 
 
--- src/libs/routes/authmiddleware.ts
+++ src/libs/routes/authmiddleware.ts
@@ -1,19 +1,17 @@
 import * as jwt from 'jsonwebtoken';
 import hasPermission from '../permission';
 import { userModel } from '../../repositories/User/UserModel';
-import config from '../../config/configuration'
+import config from '../../config/configuration';
 
 export default (module, permissionType) => (req, res, next) => {
-    try{
+    try {
         console.log('Module and permission is', module, permissionType);
         console.log('header', req.header('authorization'));
         const token = req.header('authorization');
-        //console.log(config.Secret_Key,token);
         const decode = jwt.verify(token, config.Secret_Key);
         console.log('decoded user', decode);
         console.log('email nad password', decode.email, decode.password, decode.role);
         userModel.findOne({ email: decode.email, password: decode.password }, (err, result) => {
-         // console.log("abcde",result)
             if (!result) {
                 return next({
                     error: 'User not existing in db',
@@ -30,13 +28,11 @@ export default (module, permissionType) => (req, res, next) => {
                 return next({
                     error: 'Unauthorized User role',
                     code: 403
-                })
+              });
             }
             return next();
-    
       });
-    
-    }
+     }
      catch (err) {
         next({
             error: 'Unauthorized',
